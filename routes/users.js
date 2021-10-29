499c0fa8b8121f5e78cb82d8ea78c4eec9aa2acf
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-29 14:29:20 +0100

    added user post method

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,32 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 24
+    }
+]
+
 router.get('/', (req, res) => {
+    console.log(users);
     res.send('Hello');
 });
+
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push(user);
+
+    res.send(`User with the name ${user.firstName} added to the DB`);
+});
+
+
 
 export default router;
